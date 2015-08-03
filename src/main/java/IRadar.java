6036b0198010341f7408b119ae0187255e0b4983
RobinGadik <kkpomsk@gmail.com>
2015-08-03 16:29:24 +0600

    IRadar was changed

--- src/main/java/IRadar.java
+++ src/main/java/IRadar.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kkp on 03.08.15.
  */
 public interface IRadar {
-    ArrayList<ITarget> getTargets();
+    List getTargets();
 }
