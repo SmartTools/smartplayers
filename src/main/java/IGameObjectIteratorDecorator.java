bf6379f689971da1aaca3f97eeb8dca6c55c8646
RobinGadik <kkpomsk@gmail.com>
2015-08-10 15:10:10 +0600

    class IGameObjectIteratorDecorator  was created

---
+++ src/main/java/IGameObjectIteratorDecorator.java
@@ -0,0 +1,26 @@
+import interfaces.IGameObject;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by kkp on 10.08.15.
+ */
+public class IGameObjectIteratorDecorator implements Iterator {
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public Object next() {
+        return null;
+    }
+
+    public void remove() {
+
+    }
+
+    public void add(){
+
+    }
+}
