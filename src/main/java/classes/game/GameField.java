d351ea7eee8cdc0a2e98356f66023e44dff17363
dmitry <dimaaasik.s@gmail.com>
2015-08-14 16:08:07 +0600

    Added win condition in main cycle.

--- src/main/java/classes/game/GameField.java
+++ src/main/java/classes/game/GameField.java
@@ -60,8 +60,27 @@ public class GameField implements IGameField {
 
     }
 
-    private boolean hasWinner() {
-        return false;
+    private Boolean hasWinner() {
+        Iterator<IOwnership> units = new GameObjectIteratorDecorator<>(
+                objects,
+                "remoteControl",
+                (IGameObject obj) -> (IOwnership) obj.getKey("ownership"),
+                (IGameObject obj) -> {
+                    IHealth health = (IHealth) obj.getKey("health");
+                    return health.getValue() > 0;
+                }
+        );
+        if (!units.hasNext()) {
+            return Boolean.TRUE;
+        }
+        String player = units.next().getOwner();
+        while (units.hasNext()) {
+            IOwnership unit = units.next();
+            if (!player.equals(unit.getOwner())) {
+                return Boolean.FALSE;
+            }
+        }
+        return Boolean.TRUE;
     }
 
     public Iterable<IPlayer> getPlayers() {
